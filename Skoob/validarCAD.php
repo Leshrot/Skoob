6406643f2c1435854d69768abd434102ebc4d3dc
diogocamilo <diogo_camilo_dcsantos@outlook.com>
2016-12-08 19:18:10 -0200

    Update validarCAD.php

--- Skoob/validarCAD.php
+++ Skoob/validarCAD.php
@@ -10,69 +10,58 @@
 
     # RECEBENDO VALORES DO FORMULÁRIO
 
-if (isset($_POST["submit"])) { 
-    $nmUsuario = $_POST['txtnome'];
-    $dsApelidoUsuario = $_POST['txtapelido'];
-    $sxUsuario = $_POST['opsexo'];
-    $dsUFUsuario = $_POST['opuf'];
-    $emailUsuario = $_POST['txtemail'];
-    $dsSenhaUsuario = $_POST['psdsenha'];
-}
-
-if (isset($nmUsuario, $dsApelidoUsuario, $emailUsuario)){
-
-    # SELECT COMPARANDO LOGIN E SENHA COM BANCO - ERRO
-    $query = mysql_query("SELECT * FROM tb_leitor WHERE
-        nm_leitor = '".$nmUsuario."' 
-        OR
-        nm_apelido_leitor = '".$dsApelidoUsuario."' 
-        OR
-        ds_email_leitor = '".$emailUsuario."'
-        ");
-
-    # VERIFICANDO TODAS AS LINHAS PARA ENCONTRAR USUÁRIO
-        $result = mysql_num_rows($query);
-    if($result >= 1){
-        echo "Usuário já cadastrado";
-    } else {
-        # INSERÇÃO NO BANCO DE DADOS
-        $vSQL = "INSERT INTO `tb_leitor` (`nm_leitor`, `nm_apelido_leitor`, `ds_sexo_leitor`, `ds_distrito_leitor`, `ds_email_leitor`,`ds_senha_leitor`)  VALUES ('".$nmUsuario."', '".$dsApelidoUsuario."', '".$sxUsuario."', '".$dsUFUsuario."', '".$emailUsuario."', '".$dsSenhaUsuario."')";
-
-        $result = mysql_query($vSQL);
-        if ($result) {
-          echo "Seu cadastro foi realizado com sucesso";
-
-            # INSTANCIANDO OBJETO LEITOR
-    while ($result = mysql_fetch_assoc($query)) {
-
+    if (isset($_POST["submit"])) { 
         $nmUsuario = $_POST['txtnome'];
         $dsApelidoUsuario = $_POST['txtapelido'];
         $sxUsuario = $_POST['opsexo'];
         $dsUFUsuario = $_POST['opuf'];
         $emailUsuario = $_POST['txtemail'];
         $dsSenhaUsuario = $_POST['psdsenha'];
+    }
     
-        $leitor->setNome($nmUsuario);
-        $leitor->setApelido($dsApelidoUsuario);
-        $leitor->setSexo($sxUsuario);
-        $leitor->setEmail($dsUFUsuario);
-        $leitor->setSenha($emailUsuario);
-        $leitor->setUF($dsSenhaUsuario);
-        }
+        if (isset($nmUsuario, $dsApelidoUsuario, $emailUsuario)){
     
-        # COOKIES E SESSION
-        setcookie ("usuario", $leitor->getEmail(), 3000);
-        setcookie ("senha", $leitor->getSenha(), 3000);
-        $_SESSION["UF"] = $leitor->getUF();
-        $_SESSION["nome"] = $leitor->getNome();
-
-          echo '<script>window.location="confCAD.php";</script>';
-        } else {
-          echo "Não foi possível realizar o cadastro, tente novamente.";
-          // Exibe dados sobre o erro:
-          echo "Dados sobre o erro: " . mysql_error();
+                # SELECT COMPARANDO LOGIN E SENHA COM BANCO - ERRO
+                $query = mysql_query("SELECT * FROM tb_leitor WHERE
+                nm_leitor = '".$nmUsuario."' 
+                OR
+                nm_apelido_leitor = '".$dsApelidoUsuario."' 
+                OR
+                ds_email_leitor = '".$emailUsuario."'
+                ");
+    
+                # VERIFICANDO TODAS AS LINHAS PARA ENCONTRAR USUÁRIO
+                $result = mysql_num_rows($query);
+                if($result >= 1){
+                    echo "Usuário já cadastrado";
+                } else {
+                # INSERÇÃO NO BANCO DE DADOS
+                $vSQL = "INSERT INTO `tb_leitor` (`nm_leitor`, `nm_apelido_leitor`, `ds_sexo_leitor`, `ds_distrito_leitor`, `ds_email_leitor`,`ds_senha_leitor`)  VALUES ('".$nmUsuario."', '".$dsApelidoUsuario."', '".$sxUsuario."', '".$dsUFUsuario."', '".$emailUsuario."', '".$dsSenhaUsuario."')";
+    
+                $result = mysql_query($vSQL);
+                if ($result) {
+                  echo "Seu cadastro foi realizado com sucesso";
+    
+                # INSTANCIANDO OBJETO LEITOR
+                $leitor->setNome($nmUsuario);
+                $leitor->setApelido($dsApelidoUsuario);
+                $leitor->setSexo($sxUsuario);
+                $leitor->setEmail($dsUFUsuario);
+                $leitor->setSenha($emailUsuario);
+                $leitor->setUF($dsSenhaUsuario);
+            
+                # COOKIES E SESSION
+                setcookie ("usuario", $leitor->getEmail(), 3000);
+                setcookie ("senha", $leitor->getSenha(), 3000);
+                $_SESSION["UF"] = $leitor->getUF();
+                $_SESSION["nome"] = $leitor->getNome();
+    
+            echo '<script>window.location="confCAD.php";</script>';
+            } else {
+              echo "Não foi possível realizar o cadastro, tente novamente.";
+              // Exibe dados sobre o erro:
+              echo "Dados sobre o erro: " . mysql_error();
+            }
         }
     }
-}
-
 ?>
